00477f81dc02047bcfe7edfaec8ced4af1daf0a8
Mohamed Alaa <72877626+mohamed1Alaa@users.noreply.github.com>
2023-12-04 15:36:45 +0200

    Update F - Palindromic Number.cpp

--- Level 1/Class 2/Practice G2/Week 02/F - Palindromic Number.cpp
+++ Level 1/Class 2/Practice G2/Week 02/F - Palindromic Number.cpp
@@ -17,15 +17,28 @@ int main()
 // another solution 
 
 #include <iostream>
-#include<cmath>
 using namespace std;
 int main()
 {
   int n;
   cin >> n;
-  int last = n%10;
-  int first = n/100;
-  if (last == first)
+  int rev = 0;
+  int temp = n;
+  // n = 12 , rev =  3
+  rev *= 10;
+  rev += (n%10);
+  n /= 10;
+// n = 1 , rev =  30+2 = 32
+  rev *= 10;
+  rev += (n%10);
+  n /= 10;
+// n =  0, rev =  30+2 = 321
+  rev *= 10;
+  rev += (n%10);
+  n /= 10;
+  
+  n = temp;
+  if (n == rev)
     cout << "Yes\n";
   else 
     cout << "No\n";  
