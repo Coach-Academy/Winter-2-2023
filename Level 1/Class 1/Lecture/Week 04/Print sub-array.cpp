fefc1f7ecb5495391e4e1f490e61303e26b230ce
mohamedosamaayoup202676@gmail.com <mayoub@sumerge.com>
2023-12-16 18:59:14 +0200

    solutions

---
+++ Level 1/Class 1/Lecture/Week 04/Print sub-array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int n;
+ cin >> n;
+ int arr[n];
+
+ for(int i = 0 ; i < n ; i++)
+    cin >> arr[i];
+
+ int l , r ;
+ cin >> l >> r;
+
+ for(int i = l-1 ; i <= r-1 ; i++)
+    cout << arr[i] << " ";
+
+}
