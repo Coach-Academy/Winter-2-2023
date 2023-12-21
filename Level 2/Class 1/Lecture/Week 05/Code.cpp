9d226ff5ec3e85887f85b2edfad95c3a6372f69a
Abdelrahman Elzarka <108380629+Abdelrahmanelzarka@users.noreply.github.com>
2023-12-21 23:53:22 +0200

    Create Code.cpp

---
+++ Level 2/Class 1/Lecture/Week 05/Code.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+int a=5;
+int fun2()
+{
+    int y,m;
+    if (y==0)
+    return ;
+    fun2();
+}
+
+
+int fun1()
+{
+    int z,x;
+    fun2();
+    cout<<n;
+    cout<<a;
+}
+
+*/
+int main()
+{
+   // cout<<a;
+   /* int a[100001]; /// O(n)
+    cin>>n;
+
+
+
+    int n,d,c,v,g,r; /// O(1)
+    cin>>n;
+
+    for(int i=0 ; i<n ; i++)
+        for(int j=0 ; j<n ; j++)
+        cin>>v[i][j];
+    /// O(n^2)
+    //cout<<n;
+
+
+
+
+
+    for(int i=0 ; i<n ; i++)
+        for(int j=0 ; j<n ; j*=2)
+           cout<<j;
+    /// O(n^2)
+
+    for(int i=0 ; i<n; i++)
+        cout<<i;
+    /// O(n)
+
+
+        ///O(1)
+        ///O(log2(n))
+        /// O(n)
+        /// O(nlogn)
+        /// O(n^c)
+        /// O(c^n)
+        /// O(n!)*/
+
+      /// problem 1
+     /*   int n;
+        cin>>n;
+        int sum=0;
+      /*  for(int i=1 ; i<=n ; i++)
+            sum+=i;*/
+
+        ///O(n)
+
+
+     /*   sum=(n*(n+1))/2;
+        ///O(1)
+
+        cout<<sum;
+        fun1();
+        /// problem 2*/
+
+      /*  long long  n;
+        cin>>n;
+
+
+       if(n%2==0)
+        cout<<n/2;
+       else
+        cout<<((n+1)/-2);*/
+        /// O(1)
+
+    /// Problem 3
+   /* long long int n;
+    cin>>n;
+    cout<<25;*/
+    ///O(1)
+
+
+    int * p=new int;
+    *p=10;
+    *p =5;
+    *p+=10;
+    cout<<*p<<"\n";
+    delete p;
+    delete p;
+    cout<<*p;
+
+
+
+
+
+    return 0;
+}
