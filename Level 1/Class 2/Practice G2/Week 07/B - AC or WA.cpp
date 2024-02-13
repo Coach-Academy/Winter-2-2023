357029093b00e2254a4efbca3c3780f85dd81eb3
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-02-14 00:54:40 +0200

    Create  B - AC or WA.cpp

---
+++ Level 1/Class 2/Practice G2/Week 07/B - AC or WA.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n,m;cin>>n>>m;
+    if(n==m)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+    return 0;
+}
+
