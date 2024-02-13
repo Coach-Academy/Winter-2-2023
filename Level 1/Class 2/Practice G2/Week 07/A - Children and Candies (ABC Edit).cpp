05b9c6f175eebba7da3ea252d8b794fe6bb4447b
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-02-14 00:42:34 +0200

    Create  A - Children and Candies (ABC Edit).cpp

---
+++ Level 1/Class 2/Practice G2/Week 07/A - Children and Candies (ABC Edit).cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n;cin>>n;
+    cout<<(n+1)*n/2;
+    return 0;
+}
+
