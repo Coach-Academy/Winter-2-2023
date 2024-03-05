8daf195c7c9cbc1227dc7f135fd1eca1ace394a2
Mahmoud Ayman <101561030+Mahmoud-ayman1@users.noreply.github.com>
2024-03-05 13:15:54 +0200

    Create E - Project Euler #3: Largest prime factor.cpp

---
+++ Level 2/Class 2/Practice G1/Week 10/E - Project Euler #3: Largest prime factor.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+vector<pair<ll,ll>> primeFactorze(ll n)
+{
+    vector<pair<ll,ll>> arr;
+    for(ll i=2;i*i<=n;i++)
+    {
+        int w=0;
+        while(n%i==0)
+        {
+            w++;
+            n/=i;
+        }
+        if(w)arr.push_back({i,w});
+    }
+    if(n>1)arr.push_back({n,1});
+    return arr;
+}
+signed main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int t;cin>>t;while(t--)
+    {
+        ll n;cin>>n;
+        vector<pair<ll,ll>>v=primeFactorze(n);
+        cout<<v.back().first<<'\n';
+    }
+    return 0;
+}
