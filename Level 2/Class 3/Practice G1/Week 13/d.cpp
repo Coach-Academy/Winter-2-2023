37b6f512874e98dc52f848075743e40b71f4360a
AbdoAbdelfatah <119816888+AbdoAbdelfatah@users.noreply.github.com>
2024-04-24 07:33:47 +0200

    Add files via upload

---
+++ Level 2/Class 3/Practice G1/Week 13/d.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define FAST ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;
+typedef long double ld;
+const int MOD=1e9+7, OO=0x3f3f3f3f;
+const ll LOO=0x3f3f3f3f3f3f3f3f;
+#define all(x) (x).begin(),(x).end()
+#define mm(f, x) memset(f,x,sizeof(f))
+#define f(n) for(int i=0;i<n;++i)
+#define fa(i,a,n) for(int i=(a);i<=(n);++i)
+#define  c(vec) for(auto &x:vec)cin>>x;
+#define ff(vec) for(auto &x:vec)cout<<x<<' ' ;cout<<'\n';
+#define fp(vec) for(auto &x:vec)cout<<x.first<<' '<<x.second<<'\n';
+#define debug(x) cout<<#x<<":"<<x<<endl;
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define pb push_back
+#define sz(a) (int)(a).size()
+const long double EPS=1e-7;
+const ll MOD1=1e18+7;
+int dr[]={-1, -1, -1, 0, 1, 1,  1,  0};
+int dc[]={-1,  0,  1, 1, 1, 0, -1, -1};
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
+
+bool GCD(ll n){
+    ll sum=0;
+    ll tmp=n;
+    while(n){
+        sum+=(n%10);
+        n/=10;
+    }
+
+    return __gcd(sum,tmp)>1;
+}
+
+
+signed  main(){
+    FAST
+    //freopen("game.in","rt",stdin);
+    //freopen("game.out","wt",stdout);
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+
+        if(GCD(n))cout<<n<<'\n';
+        else if(GCD(n+1))cout<<n+1<<'\n';
+        else cout<<n+2<<'\n';
+    }
+
+
+
+
+    return 0;
+}
+
