185955f10a8277d37dac4d341478e44fb6d5c455
AbdoAbdelfatah <abdofatah410@gmail.com>
2024-04-24 07:22:02 +0200

    code for practice session level2 week13 class3

---
+++ Level 2/Class 3/Practice G1/Week 13/a.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+#define FAST ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+typedef long long ll;
+typedef long double ld;
+const int MOD=1e9+7, OO=0x3f3f3f3f;
+const ll LOO=0x3f3f3f3f3f3f3f3f;
+#define all(x) (x).begin(),(x).end()
+#define mm(f, x) memset(f,x,sizeof(f))
+#define f(n) for(int i=0;i<n;++i)
+#define fa(i,a,n) for(int i=(a);i<=(n);++i)
+#define  c(vec) for(auto &x:vec)cin>>x;
+#define ff(vec) for(auto &x:vec)cout<<x<<' ' ;cout<<'\n';
+#define fp(vec) for(auto &x:vec)cout<<x.first<<' '<<x.second<<'\n';
+#define debug(x) cout<<#x<<":"<<x<<endl;
+#define yes cout<<"YES\n";
+#define no cout<<"NO\n";
+#define pb push_back
+#define sz(a) (int)(a).size()
+const long double EPS=1e-7;
+const ll MOD1=1e18+7;
+int dr[]={-1, -1, -1, 0, 1, 1,  1,  0};
+int dc[]={-1,  0,  1, 1, 1, 0, -1, -1};
+int dx[]={1,0,-1,0};
+int dy[]={0,1,0,-1};
+
+
+bool notPrime(int x){
+    for(int i=2;i*i<=x;i++){
+        if(x%i==0)return 1;
+    }
+    return 0;
+}
+
+
+signed  main(){
+    FAST
+    //freopen("game.in","rt",stdin);
+    //freopen("game.out","wt",stdout);
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+        int c0=0,c1=0,c2=0;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            c0+=(x%3==0);
+            c1+=(x%3==1);
+            c2+=(x%3==2);
+        }
+
+        cout<<max({c2-c1,c0-c2,c1-c0})<<'\n';
+
+    }
+
+
+
+    return 0;
+}
+
